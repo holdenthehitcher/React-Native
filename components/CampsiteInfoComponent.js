ae5fb1fe584e052a4152d31dc4919c228b7eb187
Holden Nakamura <holdenthehitcher@gmail.com>
2020-07-02 14:03:07 -1000

    created App; installed native-elements; added Main, CampsiteInfo, Directory; passed props to render descriptions and onPress comments

---
+++ components/CampsiteInfoComponent.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Text, View } from "react-native";
+import { Card } from "react-native-elements";
+
+function RenderCampsite({ campsite }) {
+  if (campsite) {
+    return (
+      <Card featuredTitle={campsite.name} image={require("./images/react-lake.jpg")}>
+        <Text style={{ margin: 10 }}>{campsite.description}</Text>
+      </Card>
+    );
+  }
+  return <View />;
+}
+
+export default function CampsiteInfo(props) {
+  return <RenderCampsite campsite={props.campsite} />;
+}
